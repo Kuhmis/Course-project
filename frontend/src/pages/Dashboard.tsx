4c878928226dfb833cb627498a73e70a01f57f32
Kuhmis <gustasthekidrs@gmail.com>
2025-02-28 09:37:39 +0200

    Small frontend changes

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -5,7 +5,7 @@ const Dashboard = () => {
     <div className="container">
       <h2>Dashboard</h2>
       <Link to="/templates/create">Create New Template</Link>
-      {/* Rest of your dashboard code */}
+      {}
     </div>
   );
 };
